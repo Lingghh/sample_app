8306d52da83ecb107906b519478d99a935687e00
Jero <lingh0927@hotmail.com>
2014-01-27 16:46:28 +0800

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,29 +21,40 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
+    it { should have_selector('h1', text: 'Sample App') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
-
+    
   end
+
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
+    it { should have_selector('h1', text: 'Help') }
     it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About') }
+    it { should have_selector('h1', text: 'About') }
     it { should have_title(full_title('About')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1',text: 'Contact') }
     it { should have_title(full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    click_link "Help"
+    click_link "Contact"
+    click_link "Home"
+    click_link "Sign up now!"
+    click_link "sample app"
   end
 end
